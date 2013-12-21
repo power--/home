7ec1507fe5b364a4f6d491f41aeb422b63fab738
chenbo <tnqu@hotmail.com>
2013-12-21 03:06:04 +0000

    test

--- service/src/test/java/com/goparty/client/UserAndEventTest.java
+++ service/src/test/java/com/goparty/client/UserAndEventTest.java
@@ -9,6 +9,7 @@ import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.jaxrs.provider.json.JSONProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -20,25 +21,38 @@ import com.goparty.webservice.LocationService;
 import com.goparty.webservice.UserService;
 
 public class UserAndEventTest {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private EventService eventService;
 	private UserService userService;
-	private static final String applicationURI ="http://localhost";
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final String applicationURI ="http://goparty.cloudapp.net";
+	
 	
 	@Before
 	public void setUp(){
-		List<Object> providers = new LinkedList<Object>();
+		List<Object> providers = getJsonProvider();
 		
 		eventService = JAXRSClientFactory.create(applicationURI + "/cxf/rest/", EventService.class, providers, true);
 		ClientConfiguration cfgProxy = WebClient.getConfig(eventService);
 		cfgProxy.getOutInterceptors().add(new LoggingOutInterceptor());
 		cfgProxy.getInInterceptors().add(new LoggingInInterceptor());
+
+		
 		
 		userService = JAXRSClientFactory.create(applicationURI + "/cxf/rest/", UserService.class, providers, true);
 		cfgProxy = WebClient.getConfig(userService);
 		cfgProxy.getOutInterceptors().add(new LoggingOutInterceptor());
 		cfgProxy.getInInterceptors().add(new LoggingInInterceptor());
-	}  
+	} 
+	
+	
+
+	private static List<Object> getJsonProvider(){ 
+		List<Object> providers = new LinkedList<Object>(); 
+		JSONProvider jsonProvider = new JSONProvider();
+		providers.add(jsonProvider); 
+        return providers; 
+    } 
+
 	
 	@Test
 	public void test(){
@@ -46,33 +60,29 @@ public class UserAndEventTest {
 		owner.setNickName("Bo");
 		owner.setUserName("chenb");
 		owner.setPassword("password");
-		logger.error("******START*************************************************");
-		owner = userService.create(owner);
-		logger.error("******END*************************************************");
-		logger.error("******START*************************************************");
-		owner = userService.read(owner.getId());
-		logger.error("******END*************************************************");
-		logger.error("******START*************************************************");
-		owner = userService.update(owner);
-		logger.error("******END*************************************************");
-		logger.error("******START*************************************************");
-		userService.delete(owner.getId());
-		logger.error("******END*************************************************");
-		
 		
 		User att1 = new User();
-		att1.setNickName("att1");
-		att1.setUserName("att1");
-		att1.setPassword("password");
+		owner.setNickName("att1");
+		owner.setUserName("att1");
+		owner.setPassword("password");
 		
 		User att2 = new User();
-		att2.setNickName("att2");
-		att2.setUserName("att2");
-		att2.setPassword("password");
+		owner.setNickName("att2");
+		owner.setUserName("att2");
+		owner.setPassword("password");
 		
 		owner  = userService.create(owner);
+		
+		logger.error("*******************************");
+		owner  = userService.read(owner.getId());
+		logger.error("*******************************");
+		owner.setNickName("Chen, Bo");
+		userService.update(owner);
+		
 		att1 = userService.create(att1);
 		att2 = userService.create(att2);
+		
+		
 		
 		Event event = new Event();
 		event.setOwner(owner);
@@ -97,10 +107,13 @@ public class UserAndEventTest {
 		event.setVisiblityCategory(v);
 		
 		event = eventService.create(event);
-		event = eventService.read(event.getId());
-		event = eventService.update(event);
-		eventService.delete(event.getId());
+		event.setDescription("Hi Man");
+		eventService.update(event);
 		
+		logger.error("*******************************");
+		event = eventService.read(event.getId());
+		logger.error("*******************************");
+		eventService.delete(event.getId());
 		userService.delete(att1.getId());
 		userService.delete(att2.getId());
 		userService.delete(owner.getId());
